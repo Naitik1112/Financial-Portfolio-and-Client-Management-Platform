48844d88bb6b8d460d4cf739cfede74b13eaa9d2
NaitikShah <naitik.1112004@gmail.com>
2025-08-03 13:23:19 +0530

    signup created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Home from './pages/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import MyClient from './pages/MyClient/MyClient';
 import SignIn from './pages/SignIn/Signin';
-// import SignUp from './pages/SignUp/SignUp';
+import SignUp from './pages/SignUp/SignUp';
 import Profile from './pages/Profile/Profile';
 import AddClient from './pages/addClient';
 import AddInsurance from './pages/addInsurance';
