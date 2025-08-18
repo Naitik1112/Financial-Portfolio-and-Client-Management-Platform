47353f088f3113a0393fe2b2db54b01a24e0589b
NaitikShah <naitik.1112004@gmail.com>
2025-08-19 00:55:43 +0530

    verification added commit testing

--- server/models/userModels.js
+++ server/models/userModels.js
@@ -87,13 +87,13 @@ const userSchema = new mongoose.Schema(
 
 function formatDOB(date_of_birth) {
   if (!date_of_birth) return null;
-  let dobObj = date_of_birth instanceof Date ? date_of_birth : new Date(date_of_birth);
+  let dobObj =
+    date_of_birth instanceof Date ? date_of_birth : new Date(date_of_birth);
   const day = String(dobObj.getDate()).padStart(2, '0');
   const month = String(dobObj.getMonth() + 1).padStart(2, '0');
   const year = dobObj.getFullYear();
   return `${day}/${month}/${year}`; // DD/MM/YYYY
 }
-
 
 // Virtual property to calculate age
 userSchema.virtual('age').get(function() {
@@ -175,18 +175,18 @@ async function verifyPAN({ pan, name, date_of_birth }) {
   const accessToken = await getAccessToken();
 
   // Convert Date object to DD/MM/YYYY
-  console.log("date_of_birth ",date_of_birth)
+  console.log('date_of_birth ', date_of_birth);
   const dob = formatDOB(date_of_birth);
-  console.log(accessToken);
-  console.log(dob);
-  console.log(pan);
-  console.log(process.env.API_KEY);
-  console.log(name);
+  // console.log(accessToken);
+  // console.log(dob);
+  // console.log(pan);
+  // console.log(process.env.API_KEY);
+  // console.log(name);
   const response = await axios.post(
-    "https://api.sandbox.co.in/kyc/pan/verify",
+    `${process.env.SANDBOX_HOST}/kyc/pan/verify`,
     {
       '@entity': 'in.co.sandbox.kyc.pan_verification.request',
-      pan : pan,
+      pan: pan,
       name_as_per_pan: name,
       date_of_birth: dob,
       consent: 'Y',
@@ -194,9 +194,8 @@ async function verifyPAN({ pan, name, date_of_birth }) {
     },
     {
       headers: {
-        Authorization:
-          'eyJ0eXAiOiJKV1MiLCJhbGciOiJSU0FTU0FfUFNTX1NIQV81MTIiLCJraWQiOiIwYzYwMGUzMS01MDAwLTRkYTItYjM3YS01ODdkYTA0ZTk4NTEifQ.eyJ3b3Jrc3BhY2VfaWQiOiIyMDU3N2Q4My1jMGYwLTRjNjYtYjZlNi1kMzA0OGM3NzBmMjgiLCJzdWIiOiJrZXlfbGl2ZV85ODJjZjE4M2EzNjU0YjllOTY2ZjQ3ODkxNzk3ZDdhZSIsImFwaV9rZXkiOiJrZXlfbGl2ZV85ODJjZjE4M2EzNjU0YjllOTY2ZjQ3ODkxNzk3ZDdhZSIsImF1ZCI6IkFQSSIsImludGVudCI6IkFDQ0VTU19UT0tFTiIsImlzcyI6InByb2QxLWFwaS5zYW5kYm94LmNvLmluIiwiaWF0IjoxNzU1NTM0OTM1LCJleHAiOjE3NTU2MjEzMzV9.vg6IBzYHhLG8EdwusLW4VpBgslJqWIPkeFBtTJcKYa1wtGcwCr8MeQncVeaFWo5s3gXo8i4DS3JhwHQxmuOYvUEJrffa_NDSad0pXdO9ttstXCUFVp2Wztaic-U1EKxPs9lqWmYLc3ih7dVXQ-jOJTjJ3cv3X4Ef-by1lm8zDsyRtQ_6ZFcQ8r2toEgm4mjZok_UcR4BcXUue72WF1Pfk3Nk8zLTC_JSLSyeYA3B0p5Ooq_QomGYzzwFkmDF8rebvo3i1vP5HY-m8THNydG5drej-PWwXqefXZaEjGO9MZibAgcaaEFaiaM_lBvE0hKX3yPu3FURa918b5NMsPEW8w', // <-- add Bearer
-        'x-api-key': 'key_live_982cf183a3654b9e966f47891797d7ae',
+        Authorization: accessToken,
+        'x-api-key': process.env.API_KEY,
         'Content-Type': 'application/json'
       }
     }
@@ -244,6 +243,8 @@ userSchema.pre('findOneAndUpdate', async function(next) {
     next(err); // Will stop update and throw error
   }
 });
+
+userSchema.index({ pancard: 1, adminId: 1 }, { unique: true });
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
